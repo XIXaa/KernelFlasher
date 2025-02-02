1c26c84bd92eb10e1e2dcf8cef6541159d274717
XIXaa <76633673+XIXaa@users.noreply.github.com>
2025-02-02 14:50:57 +0800

    Add files via upload

--- app/src/main/java/com/github/XIXaa/kernelflasher/common/types/backups/Backup.kt
+++ app/src/main/java/com/github/XIXaa/kernelflasher/common/types/backups/Backup.kt
@@ -1,1 +1,15 @@
+package com.github.XIXaa.kernelflasher.common.types.backups
 
+import com.github.XIXaa.kernelflasher.common.types.partitions.Partitions
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Backup(
+    val name: String,
+    val type: String,
+    val kernelVersion: String,
+    val bootSha1: String? = null,
+    val filename: String? = null,
+    val hashes: Partitions? = null,
+    val hashAlgorithm: String? = null
+)
