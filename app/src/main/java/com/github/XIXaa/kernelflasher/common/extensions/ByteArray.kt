9f63c0c5ec133f2fbf9269a12b0ec30495230717
XIXaa <76633673+XIXaa@users.noreply.github.com>
2025-02-02 14:49:03 +0800

    Add files via upload

--- app/src/main/java/com/github/XIXaa/kernelflasher/common/extensions/ByteArray.kt
+++ app/src/main/java/com/github/XIXaa/kernelflasher/common/extensions/ByteArray.kt
@@ -1,1 +1,7 @@
+package com.github.XIXaa.kernelflasher.common.extensions
 
+import kotlin.ByteArray
+
+object ByteArray {
+    fun ByteArray.toHex(): String = joinToString(separator = "") { "%02x".format(it) }
+}
