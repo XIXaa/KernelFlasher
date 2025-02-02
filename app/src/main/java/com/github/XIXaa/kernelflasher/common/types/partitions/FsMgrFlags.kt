e975a5bbf66ecd369f5e202cc6fd76e07f1f1626
XIXaa <76633673+XIXaa@users.noreply.github.com>
2025-02-02 14:52:13 +0800

    Add files via upload

--- app/src/main/java/com/github/XIXaa/kernelflasher/common/types/partitions/FsMgrFlags.kt
+++ app/src/main/java/com/github/XIXaa/kernelflasher/common/types/partitions/FsMgrFlags.kt
@@ -1,1 +1,8 @@
+package com.github.XIXaa.kernelflasher.common.types.partitions
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FsMgrFlags(
+    val logical: Boolean = false
+)
